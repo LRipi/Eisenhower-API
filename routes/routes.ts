022f53321c46baae7e9cd0a5d2629ab30331d217
Guillaume LE MARTRET <guilemartret@gmail.com>
2019-12-18 22:05:56 +0100

    test post-merge hook

--- routes/routes.ts
+++ routes/routes.ts
@@ -40,7 +40,7 @@ export = function (app: express.Application) {
     });
 
     app.get('/', function (req: express.Request, res: express.Response) {
-        res.send('API is alive ! (TEST)');
+        res.send('API is alive ! (TEST1)');
     });
 
     app.use(function(req: express.Request, res: express.Response, next: express.NextFunction) {
