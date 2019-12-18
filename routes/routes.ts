0423cb9f869efa127d4b70d4d3b419aa8d623110
Guillaume LE MARTRET <guilemartret@gmail.com>
2019-12-18 23:17:30 +0100

    removed hook

--- routes/routes.ts
+++ routes/routes.ts
@@ -40,7 +40,7 @@ export = function (app: express.Application) {
     });
 
     app.get('/', function (req: express.Request, res: express.Response) {
-        res.send('API is alive ! (TEST4)');
+        res.send('API is alive ! (TEST5)');
     });
 
     app.use(function(req: express.Request, res: express.Response, next: express.NextFunction) {
